e913afc3427c5a47f9507c57b67c7b81a5e1b541
Vincent Golle <v.golle@isispace.nl>
2023-07-12 17:29:10 +0200

    Added newer library

--- FT232H_GPIO.cpp
+++ FT232H_GPIO.cpp
@@ -4,30 +4,43 @@
 #include <chrono>
 #include <thread>
 
-#include <libMPSSE_i2c.h>
+#include <ftd2xx.h>
+#include <libmpsse_i2c.h>
 
 using namespace std;
 
-// Note that pin numbers 0 to 15 map to pins D0 to D7 then C0 to C9 on the board.
+#define APP_CHECK_STATUS(X)     if ((X)) {printf("Error %u line %u", (X), __LINE__);}
 
-#define PIN_C(X)     (8+(X))
+// In MPSSE I2C ADBUS is used for synchronous serial communications (I2C/SPI/JTAG) and ACBUS is free to be used as GPIO
+
+#define PIN_C(X)     (X)
 #define PIN_D(X)     (X)
+
+FT_STATUS i2c_read(FT_HANDLE ftHandle, UCHAR address, UCHAR reg, PUCHAR value);
+FT_STATUS i2c_read_multi(FT_HANDLE ftHandle, UCHAR address, UCHAR reg, PUCHAR value, UCHAR length);
+FT_STATUS i2c_write(FT_HANDLE ftHandle, UCHAR address, UCHAR reg, UCHAR value);
 
 int main()
 {
 	FT_DEVICE_LIST_INFO_NODE devList;
-	FT_STATUS status;
 	FT_HANDLE ftHandle;
-	uint32 channels = 0;
-	Init_libMPSSE();
-	FT_STATUS ftStatus = I2C_GetNumChannels(&channels);
-	cout << "Number of channels: %d" << channels <<endl;
+    FT_STATUS ftStatus;
+    DWORD channels = 0;
+
+    Init_libMPSSE();
+
+    {
+        ftStatus = I2C_GetNumChannels(&channels);
+        APP_CHECK_STATUS(ftStatus);
+        cout << "Number of channels: " << channels << endl;
+    }
 
 	if (channels > 0)
 	{
-		for (int i = 0; i < channels; i++)
+		for (DWORD i = 0; i < channels; i++)
 		{
-			status = I2C_GetChannelInfo(i, &devList);
+            ftStatus = I2C_GetChannelInfo(i, &devList);
+            APP_CHECK_STATUS(ftStatus);
 			printf("Information on channel number %d:\n", i);
 			/*print the dev info*/
 			cout << " Flags = 0x" << std::hex << devList.Flags << endl;
@@ -36,36 +49,134 @@ int main()
 			cout << " LocId = 0x" << std::hex << devList.LocId << endl;
 			cout << " SerialNumber = " << devList.SerialNumber << endl;
 			cout << " Description = " << devList.Description << endl;
-			cout << " ftHandle = " << devList.ftHandle << endl;/*is 0 unless open*/
+			cout << " ftHandle = " << devList.ftHandle << " (should be zero)" << endl;/*is 0 unless open*/
 		}
 	}
 
-	// I2C init
-	status = I2C_OpenChannel(0, &ftHandle);
+    // I2C init
+    {
+        ftStatus = I2C_OpenChannel(0, &ftHandle);
+        APP_CHECK_STATUS(ftStatus);
+    }
 
-	ChannelConfig channelConf;
-	channelConf.ClockRate = I2C_CLOCK_FAST_MODE;/*i.e. 400000 KHz*/
-	channelConf.LatencyTimer = 255;
-	status = I2C_InitChannel(ftHandle, &channelConf);
-	cout << "Init Channel. Status: " << status << endl;
+    {
+        ChannelConfig channelConf;
+        channelConf.ClockRate = I2C_CLOCK_STANDARD_MODE;
+        channelConf.LatencyTimer = 10;
+        channelConf.Options = 0;
+
+        ftStatus = I2C_InitChannel(ftHandle, &channelConf);
+        APP_CHECK_STATUS(ftStatus);
+        cout << "Init Channel. Status: " << ftStatus << endl;
+    }
 
     // GPIO toggle
-	int pin = PIN_C(0);
-	for (int i = 0; i < 10; i++)
-	{
-		FT_WriteGPIO(ftHandle, 1 << pin, 1 << pin);
-		cout << "> ON" << endl;
-		std::this_thread::sleep_for(1s);
+    uint8_t pin = PIN_C(0);
+    for (int i = 0; i < 10; i++)
+    {
+        ftStatus = FT_WriteGPIO(ftHandle, 1, 0xff);
+        APP_CHECK_STATUS(ftStatus);
+        cout << "> ON" << endl;
+        std::this_thread::sleep_for(1s);
 
-		FT_WriteGPIO(ftHandle, 1 << pin, 0);
-		cout << "> OFF" << endl;
-		std::this_thread::sleep_for(1s);
-	}
+        ftStatus = FT_WriteGPIO(ftHandle, 1, 0);
+        APP_CHECK_STATUS(ftStatus);
+        cout << "> OFF" << endl;
+        std::this_thread::sleep_for(1s);
+    }
 
-	status = I2C_CloseChannel(ftHandle);
-	Cleanup_libMPSSE();
+#ifdef USE_I2C
+
+    #define BME280_REGISTER_DEVICE_ID 0xd0
+
+    UCHAR address = 0x76;
+	UCHAR rev;
+	UCHAR reg;
+	UCHAR regbuf[32];
+	FT_STATUS status;
+
+    status = i2c_read(ftHandle, address, BME280_REGISTER_DEVICE_ID, &rev);
+    APP_CHECK_STATUS(status);
+#endif
+
+    ftStatus = I2C_CloseChannel(ftHandle);
+    APP_CHECK_STATUS(ftStatus);
+
+    Cleanup_libMPSSE();
 
 	cout << "Press Enter to Continue";
 	cin.ignore();
+
 	return 0;
+}
+
+FT_STATUS i2c_read(FT_HANDLE ftHandle, UCHAR address, UCHAR reg, PUCHAR value)
+{
+    FT_STATUS status;
+    DWORD xfer = 0;
+
+    /* As per Bosch BME280 Datasheet Figure 9: I2C Multiple Byte Read. */
+    status = I2C_DeviceWrite(ftHandle, address, 1, &reg, &xfer,
+                             I2C_TRANSFER_OPTIONS_START_BIT |
+                             I2C_TRANSFER_OPTIONS_BREAK_ON_NACK);
+
+    if (status == FT_OK)
+    {
+        /* Repeated Start condition generated. */
+        status = I2C_DeviceRead(ftHandle, address, 1, value, &xfer,
+                                I2C_TRANSFER_OPTIONS_START_BIT |
+                                I2C_TRANSFER_OPTIONS_STOP_BIT |
+                                I2C_TRANSFER_OPTIONS_NACK_LAST_BYTE);
+    }
+    APP_CHECK_STATUS(status);
+
+    return status;
+}
+
+FT_STATUS i2c_read_multi(FT_HANDLE ftHandle, UCHAR address, UCHAR reg, PUCHAR value, UCHAR length)
+{
+    FT_STATUS status;
+    DWORD xfer = 0;
+
+    /* As per Bosch BME280 Datasheet Figure 9: I2C Multiple Byte Read. */
+    status = I2C_DeviceWrite(ftHandle, address, 1, &reg, &xfer,
+                             I2C_TRANSFER_OPTIONS_START_BIT |
+                             I2C_TRANSFER_OPTIONS_STOP_BIT |
+                             I2C_TRANSFER_OPTIONS_BREAK_ON_NACK);
+
+    if (status == FT_OK)
+    {
+        /* Repeated Start condition generated. */
+        status = I2C_DeviceRead(ftHandle, address, length, value, &xfer,
+                                I2C_TRANSFER_OPTIONS_START_BIT |
+                                I2C_TRANSFER_OPTIONS_STOP_BIT |
+                                I2C_TRANSFER_OPTIONS_NACK_LAST_BYTE);
+    }
+    APP_CHECK_STATUS(status);
+
+    return status;
+}
+
+FT_STATUS i2c_write(FT_HANDLE ftHandle, UCHAR address, UCHAR reg, UCHAR value)
+{
+    FT_STATUS status;
+    DWORD xfer = 0;
+
+    /* As per Bosch BME280 Datasheet Figure 9: I2C Multiple Byte Write (not autoincremented) */
+    status = I2C_DeviceWrite(ftHandle, address, 1, &reg, &xfer,
+                             I2C_TRANSFER_OPTIONS_START_BIT |
+                             I2C_TRANSFER_OPTIONS_BREAK_ON_NACK);
+    APP_CHECK_STATUS(status);
+
+    if (status == FT_OK)
+    {
+        /* Register address not sent on register write. */
+        status = I2C_DeviceWrite(ftHandle, address, 1, &value, &xfer,
+                                 I2C_TRANSFER_OPTIONS_NO_ADDRESS |
+                                 I2C_TRANSFER_OPTIONS_STOP_BIT |
+                                 I2C_TRANSFER_OPTIONS_BREAK_ON_NACK);
+        APP_CHECK_STATUS(status);
+    }
+
+    return status;
 }
